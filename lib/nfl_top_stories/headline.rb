77e16b62981fc37c5e9eeddf71d54cefa1c4841c
thebrianemory <the@brianemory.com>
2016-04-28 21:43:33 -0700

    Make ESPN tweaks, add NFL stories

--- lib/nfl_top_stories/headline.rb
+++ lib/nfl_top_stories/headline.rb
@@ -52,52 +52,60 @@ class Headline
   end
 
   ## ------------------- NFL.com Headlines ------------------- ##
-    def self.nfl_headlines
-      @nfl_headlines = []
-      self.scrape_nfl
+  def self.nfl_headlines
+    @nfl_headlines = []
+    self.scrape_nfl
+    self.nfl_choice
+  end
+
+  def self.scrape_nfl
+    main_url = "http://www.nfl.com"
+    doc = Nokogiri::HTML(open("http://www.nfl.com/news"))
+
+    i = 0
+    while i < 10
+      nfl_headline = self.new
+      nfl_headline.title = doc.search("div#headlines-latest li a")[i].text
+      nfl_headline.url = main_url +  doc.search("div#headlines-latest li a")[i].attributes["href"].value
+      @nfl_headlines << nfl_headline
+      i += 1
+    end
+  end
+
+  def self.nfl_choice
+    puts ""
+    puts "\033[0;36mWhich story would you like to read?\033[0m"
+    puts "0. Back to the main menu"
+    @nfl_headlines.each.with_index(1) do |headline, i|
+      puts "#{i}. #{headline.title}"
+    end
+    input = gets.strip.downcase
+    if input == "0"
+      puts ""
+      CLI.new.call
+    elsif input == "1"
+      Story.nfl_stories(@nfl_headlines[0].url)
+    elsif input == "2"
+      Story.nfl_stories(@nfl_headlines[1].url)
+    elsif input == "3"
+      Story.nfl_stories(@nfl_headlines[2].url)
+    elsif input == "4"
+      Story.nfl_stories(@nfl_headlines[3].url)
+    elsif input == "5"
+      Story.nfl_stories(@nfl_headlines[4].url)
+    elsif input == "6"
+      Story.nfl_stories(@nfl_headlines[5].url)
+    elsif input == "7"
+      Story.nfl_stories(@nfl_headlines[6].url)
+    elsif input == "8"
+      Story.nfl_stories(@nfl_headlines[7].url)
+    elsif input == "9"
+      Story.nfl_stories(@nfl_headlines[8].url)
+    elsif input == "10"
+      Story.nfl_stories(@nfl_headlines[9].url)
+    else
+      puts "Invalid entry..."
       self.nfl_choice
     end
-
-    def self.scrape_nfl
-      main_url = "http://www.nfl.com"
-      doc = Nokogiri::HTML(open("http://www.nfl.com/news"))
-      i = 0
-      while i < 6
-        nfl_headline = self.new
-        nfl_headline.title = doc.search("div.headlines ul li a")[i]
-        nfl_headline.url = main_url +  doc.search("div.news-contents ol li div div a")
-        @nfl_headlines << nfl_headline
-        i += 1
-      end
-    end
-
-    def self.nfl_choice
-      puts ""
-      puts "\033[0;36mWhich story would you like to read?\033[0m"
-      puts "0. Back to the main menu"
-      puts "#{@nfl_headlines}"
-      # @nfl_headlines.each.with_index(1) do |headline, i|
-      #   puts "#{i}. #{headline.title}"
-      # end
-      # input = gets.strip.downcase
-      # if input == "0"
-      #   puts ""
-      #   CLI.new.call
-      # elsif input == "1"
-      #   Story.nfl_stories(@nfl_headlines[0].url)
-      # elsif input == "2"
-      #   Story.nfl_stories(@nfl_headlines[1].url)
-      # elsif input == "3"
-      # Story.nfl_stories(@nfl_headlines[2].url)
-      # elsif input == "4"
-      # Story.nfl_stories(@nfl_headlines[3].url)
-      # elsif input == "5"
-      # Story.nfl_stories(@nfl_headlines[4].url)
-      # elsif input == "6"
-      # Story.nfl_stories(@nfl_headlines[5].url)
-      # else
-      #   puts "Invalid entry..."
-      #   self.nfl_choice
-      # end
-    end
+  end
 end
--- lib/nfl_top_stories/story.rb
+++ lib/nfl_top_stories/story.rb
@@ -1,33 +1,68 @@
 class Story
   attr_accessor :title, :author, :posted, :body
 
-  def self.espn_stories(url)
-    doc = Nokogiri::HTML(open(url))
-    new_doc = doc.at("aside").remove
-    story = self.new
-    story.title = doc.search("header.article-header h1").text
-    story.author = doc.search("div.author span").text
-    story.body = doc.css("div.article-body p")
-    new_stories = []
-    story.body.each do |x|
-      new_stories << x.text unless x.text == ""
+  def self.keep_reading(source)
+    puts ""
+    puts "Would you like to read more stories?"
+    input = gets.strip.downcase
+    if input == "yes" || input == "y"
+      case source.to_s
+      when "espn"
+        Headline.espn_choice
+      when "nfl"
+        Headline.nfl_choice
+      end
+    else
+      CLI.new.goodbye
     end
+  end
 
+  def self.display_story(source)
     puts <<~DOC
-    \033[1;34m#{story.title}\033[0m
-    \033[0;31mStory by #{story.author}\033[0m
+    \033[1;34m#{@story.title}\033[0m
+    \033[0;31mStory #{@story.author}\033[0m
     DOC
-      new_stories[0,5].each do |para|
+      @new_stories[0,5].each do |para|
         puts <<~DOC
         #{para}\n
         DOC
       end
     puts <<~DOC
-    Read the rest of the story at \033[4;35m#{url}\033[0m
+    Read the rest of the story at \033[4;35m#{@url}\033[0m
     DOC
-    Headline.espn_choice
+    self.keep_reading(source)
   end
 
-  def self.nfl_stories
+  def self.espn_stories(url)
+    doc = Nokogiri::HTML(open(url))
+    new_doc = doc.at("aside").remove
+    @url = url
+    @story = self.new
+    @story.title = doc.search("header.article-header h1").text
+    @story.author = "by " + doc.search("div.author span").text
+    @story.body = doc.search("div.article-body p")
+    @new_stories = []
+    @story.body.each do |x|
+      @new_stories << x.text unless x.text == ""
+    end
+    self.display_story("espn")
+  end
+
+  def self.nfl_stories(url)
+    doc = Nokogiri::HTML(open(url))
+    @url = url
+    @story = self.new
+    @story.title = doc.search("div#article-hdr h1").text
+    @story.author = doc.search("li#article-hdr-meta-author").text.strip
+    @story.body = doc.search("div.articleText p")
+    @new_stories = []
+    @story.body.each do |x|
+      if x.content.strip.empty?
+        x.remove
+      else
+        @new_stories << x.text
+      end
+    end
+    self.display_story("nfl")
   end
 end
